48b87dcd911157da8da1f45f7e8df2eafb788167
alberth270 <aba.27.0309@gmail.com>
2020-10-23 19:52:26 -0500

    version-dockerizada-azure

---
+++ target/classes/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO persons(documento,fingerprint,blacklist) VALUES('10000000',true,false);
+INSERT INTO persons(documento,fingerprint,blacklist) VALUES('10000001',false,false);
+INSERT INTO persons(documento,fingerprint,blacklist) VALUES('10000002',false,false);
+INSERT INTO persons(documento,fingerprint,blacklist) VALUES('10000003',true,false);
+INSERT INTO persons(documento,fingerprint,blacklist) VALUES('10000004',false,true);
+INSERT INTO persons(documento,fingerprint,blacklist) VALUES('10000005',true,false);
