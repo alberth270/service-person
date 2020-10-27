8eee234f3ebbecd2bc1115b149cce146fe66e759
alberth270 <aba.27.0309@gmail.com>
2020-10-26 19:17:04 -0500

    version sin config-server

--- src/main/java/com/everis/proyect/repository/PersonRepository.java
+++ src/main/java/com/everis/proyect/repository/PersonRepository.java
@@ -4,7 +4,6 @@ import com.everis.proyect.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
   public Person findByDocumento(String documento);
--- src/main/java/com/everis/proyect/services/BaseService.java
+++ src/main/java/com/everis/proyect/services/BaseService.java
@@ -4,7 +4,8 @@ import com.everis.proyect.entity.Person;
 import io.reactivex.Single;
 
 public interface BaseService<E> {
-  public Single<E> findbyDocumento(String documento);
-
   public Single<Person> update(String documento, Person entity);
+    
+  public Single<E> findbyDocumento(String documento);
+  
 }
